abe9331054878fc887d569dca7ddb884fee13f91
sangyoonk <44706447+sangyoonk@users.noreply.github.com>
2023-04-25 12:22:33 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+graph = []
+
+for _ in range(N):
+    graph.append(list(map(int, input().rstrip()))) 
+    # readline의 경우 맨 뒤에 '\n'까지 입력받으므로 제거해줘야 함
+
+def bfs(x, y):
+    q=deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i] 
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                q.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph[N-1][M-1]
+
+print(bfs(0,0))

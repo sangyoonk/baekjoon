b0a93a0ee4e08b31e2e48a009bd04fff684a8492
sangyoonk <44706447+sangyoonk@users.noreply.github.com>
2023-09-25 00:19:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+h, m = map(int, input().split())
+t = int(input())
+
+h = h + t // 60
+m = m + t % 60
+
+if m >= 60:
+  h = h + 1
+  m = m - 60
+if h >= 24:
+  h = h - 24
+
+print(h, m)
